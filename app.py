c89bb9d180ebd4678f24bfb5fc09864579e64576
Ekpu Chinwe <ekpuchinwegodbless@gmail.com>
2026-04-16 17:53:41 -0700

    Changed site identity to lee jun eun and added 404 page

--- app.py
+++ app.py
@@ -227,5 +227,9 @@ def admin_send():
     return jsonify({"status": "sent"})
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
